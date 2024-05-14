5870d2e3718abc849b5b3e4de24c7a7d64c46305
m_HG_m <73879765+HG-dev17@users.noreply.github.com>
2024-05-15 00:51:32 +0800

    Add files via upload

---
+++ abot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,23 @@
+# CMake generated Testfile for 
+# Source directory: /home/abot/abot_ws/src
+# Build directory: /home/abot/abot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("abot_base/abot_imu")
+subdirs("abot_base/abot_bringup")
+subdirs("color_pkg")
+subdirs("face_pkg")
+subdirs("abot_base/lidar_filters")
+subdirs("opencv_demo")
+subdirs("robot_slam")
+subdirs("robot_voice")
+subdirs("abot_base/abot_model")
+subdirs("shoot_cmd")
+subdirs("cam_track")
+subdirs("imu_filter")
+subdirs("abot_find")
+subdirs("track_tag")
+subdirs("tracker_pkg")
+subdirs("user_demo")
